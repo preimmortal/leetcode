c9e9febb5eb8a9bc636e391c102105f14ca7b1d7
david <preimmortal@gmail.com>
2015-07-01 05:31:03 -0700

    done with BST

--- Tree/tree.cpp
+++ Tree/tree.cpp
@@ -126,18 +126,130 @@ void tree::treeInsert(int val){
     }
 }
 
-//TODO: Finish Delete
 void tree::treeDelete(int val){
+    //Identify node
+    node *n = root;
+    node *parent = NULL;
+    bool found = false;
+    while(n != NULL){
+        if(val == n->val){
+            found = true;
+            break;
+        }
+        parent = n;
+        //Value is less, go left
+        if(val < n->val){
+            n = n->left;
+        }
+        //Value is greater, go right
+        else{
+            n = n->right;
+        }
+    }
+
+    if(!found){
+        cout << "Value " << val << " not found in tree" << endl;
+        return;
+    }
+
+    //Found node to delete
+    else{
+        //Case: Leaf Node
+        if(n->left == NULL && n->right == NULL){
+            //If node is root
+            if(parent == NULL){
+                node *t = n;
+                root = NULL;
+                delete n;
+                return;
+            }
+            //Node is not root
+            else{
+                if(parent->left == n) parent->left = NULL;
+                else parent->right = NULL;
+                delete n;
+                return;
+            }
+        }
+        //Case: 2 Children
+        if(n->left != NULL && n->right != NULL){
+            if(n->left->right != NULL){
+                //Find max of left child and replace node
+                //then delete max child
+                node *t = n->left;
+                node *max_left_p = parent;
+                node *max_left = n;
+                while(t != NULL){
+                    max_left_p = max_left;
+                    max_left = t;
+                    t = t->right;
+                }
+                n->val = max_left->val;
+                max_left_p->right = NULL;
+                delete max_left;
+            }
+            else{
+                //Max of left child is the left child
+                //Set equal to left child and delete left child
+                node *t = n->left;
+                n->val = t->val;
+                n->left = t->left;
+                delete t;
+            }
+            return;
+        }
+        else{
+            //Case: 1 Child
+            //If node is root
+            if(parent == NULL){
+                if(n->left != NULL){
+                    root = n->left;
+                }
+                else{
+                    root = n->right;
+                }
+                delete n;
+            }
+
+            else{
+                //Replace with child
+                if(n->left != NULL){
+                    if(parent->left == n)
+                        parent->left = n->left;
+                    else
+                        parent->right = n->left;
+                    delete n;
+                }else{
+                    if(parent->left == n)
+                        parent->left = n->right;
+                    else
+                        parent->right = n->right;
+                    delete n;
+                }
+                return;
+            }
+        }
+    }
     
 }
 
 int main(){
+
+    /*
     tree T(5);
     T.treeInsert(3);
     T.treeInsert(7);
     T.treeInsert(4);
     T.treeInsert(6);
+    T.treeInsert(10);
+
+    T.treeDelete(5);
     T.treePrint(T.getRoot());
-    //out << T.treeSearch(T.getRoot(),0) << endl;
+    */
+
+    tree T(5);
+    T.treeInsert(4);
+    T.treeDelete(5);
+    T.treePrint(T.getRoot());
     return 0;
 }
